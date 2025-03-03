e072a3261e147e2890686bb2cd8e1a004008d228
sh3073 <163155403+sh3073@users.noreply.github.com>
2025-03-03 15:29:13 +0000

    Update params.py

--- sims/params.py
+++ sims/params.py
@@ -3,16 +3,17 @@ import utm
 from utm import *
 
 # path relative to the root dir of this template. Leave as mesh/blah.msh in most cases
-mesh_file = 'mesh/my_mesh.msh'
+# added my file and data
+mesh_file = 'celtic_sea.msh'
 forcing_boundary = 666
-utm_zone = 56
-utm_band="K"
-cent_lat = -24.15
-cent_lon = 151.8
-spin_up = 432000 # 5 days
-end_time = 3456000 # 40 days
+utm_zone = 30
+utm_band="U"
+cent_lat = -592967
+cent_lon = 6703824
+spin_up = 172800  # 2 days in seconds
+end_time = 2764800 # 32 days
 output_dir = "output"
-output_time = 900
+output_time = 900 #outputs generated every 15 mins
 constituents = ['M2', 'S2', 'N2', 'K2', 'K1', 'O1', 'P1', 'Q1', 'M4']
 # year, month, day, hour, min, sec
 start_datetime = datetime.datetime(2000,1,1,0,0,0) 
